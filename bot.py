e43395e54f1bc0b25ea2f3b70457916ddf2e5b62
Julien Roullé <jroulle@aquassist.com>
2019-09-10 11:53:11 -0400

    feat(bot): bot instantiation

---
+++ bot.py
@@ -0,0 +1,17 @@
+import os
+
+import discord
+from discord.ext import commands
+
+from dotenv import load_dotenv
+
+load_dotenv()
+token = os.getenv('DISCORD_TOKEN')
+
+bot = commands.Bot(command_prefix='!')
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} has connected to Discord!')
+
+bot.run(token)
